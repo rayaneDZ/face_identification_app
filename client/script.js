b31121fb622dd81be895679d76137c03a38a4d01
rayaneDZ <chlef-algerie@hotmail.fr>
2019-06-07 18:49:37 +0100

    added interaction with the model

---
+++ client/script.js
@@ -0,0 +1,131 @@
+(getUsers = () => {
+    console.log('getting the users')
+    axios.get('http://192.168.1.5:5000/api/users')
+    .then(result => {
+        const options = result.data.users.map(user => `<option value="${user.username}">${user.username}</option>`)
+        document.querySelector('select').innerHTML = options
+    }).catch(err =>{
+    console.log(err);
+    })
+})()
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDyvR4SeXocW5gooY6vfvtfuDfahnPJ7C8",
+    authDomain: "face-identification-c2987.firebaseapp.com",
+    databaseURL: "https://face-identification-c2987.firebaseio.com",
+    projectId: "face-identification-c2987",
+    storageBucket: "face-identification-c2987.appspot.com",
+    messagingSenderId: "86919330306",
+    appId: "1:86919330306:web:ba1413e0780a2cf3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+document.getElementById('upload_image_btn').addEventListener('click', () => {
+    document.getElementById('add_user_container').style.display = 'none';
+    document.getElementById('upload_image_container').style.display = "block";
+})
+
+document.getElementById('add_user_btn').addEventListener('click', () => {
+    document.getElementById('add_user_container').style.display = 'block';
+    document.getElementById('upload_image_container').style.display = "none";
+})
+
+document.getElementById('select_image_btn').addEventListener('click', () => {
+    document.getElementById('file_input').click();
+})
+
+document.getElementById('file_input').addEventListener('change', (e) => {
+    const image = e.target.files[0]
+    var reader  = new FileReader();
+    reader.readAsDataURL(image);
+    reader.addEventListener("load", function () {
+        const src = reader.result;
+        document.getElementById('image_preview').innerHTML = `<img src="${src}" width="100%" alt="Image preview..." id="imagePreview" style="margin-bottom : 10px"/>`
+    }, false);
+    document.getElementById('upload_image_must_select_image').style.display = "none"
+})
+
+document.getElementById('add_user_to_backend_btn').addEventListener('click', () => {
+    const username = document.getElementById('add_user_name').value;
+    const secret_key = document.getElementById('add_user_secret_key').value;
+    axios.post('http://192.168.1.5:5000/api/add_user', {
+        username : username.toLowerCase(),
+        secret_key : secret_key
+    }).then(() => {
+        document.getElementById('add_user_wrong_secret_key').style.display = 'none';
+        document.getElementById('add_user_user_exists').style.display = 'none';
+        document.getElementById('add_user_container').style.display = 'none';
+	document.getElementById('upload_image_container').style.display = "block";
+        alert('User Added !')
+        getUsers();
+    }).catch((err) => {
+        const code = err.response.data.code;
+        console.log(err)
+        if(code === 1){
+            document.getElementById('add_user_wrong_secret_key').style.display = 'none';
+            document.getElementById('add_user_user_exists').style.display = 'block';
+        }else if ( code === 0){
+            document.getElementById('add_user_wrong_secret_key').style.display = 'block';
+            document.getElementById('add_user_user_exists').style.display = 'none';
+        }
+    })
+})
+
+document.getElementById('upload_image_to_backend_btn').addEventListener('click', () => {
+    const image = document.getElementById('file_input').files[0];
+    const username = document.querySelector('select').value;
+    const secret_key = document.getElementById('upload_image_secret_key').value;
+    const image_uuid = uuid()
+    if(image){
+        document.getElementById('upload_image_must_select_image').style.display = "none"
+        axios.post('http://192.168.1.5:5000/api/check_auth', {
+            secret_key : secret_key
+        })
+        .then(() => {
+            document.getElementById('upload_image_wrong_secret_key').style.display = 'none';
+            progress = document.getElementById('progress');
+            const uploadTask = firebase.storage().ref().child(`${username}/${image_uuid}`).put(image);
+            progress.style.display = 'block';
+            uploadTask.on("state_changed", (snapshot) => {
+                let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                document.getElementById('determinate').style.width = progress + '%'
+            }, () =>{
+                console.log('error occured in uploadtaks to firebase')
+            }, () => {
+                // Upload completed successfully, now we can get the download URL
+                uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+                    axios.post('http://192.168.1.5:5000/api/upload_picture', {
+                        username : username,
+                        image_path : downloadURL
+                    }).then(() => {
+                        progress.style.display = 'none';
+                        document.getElementById('imagePreview').style.display = 'none';
+                        alert('Image Added !')
+                    })
+                });
+            });
+            //add image url to backend
+        }).catch(err => {
+            if(err.response){
+                document.getElementById('upload_image_wrong_secret_key').style.display = 'block';
+            }
+        })
+    }else{
+        document.getElementById('upload_image_must_select_image').style.display = "block"
+    }
+})
+
+document.getElementById('train_btn').addEventListener('click', () => {
+    console.log('training the model ...')
+    axios.get('http://192.168.1.5:5000/api/train')
+    .then(result => {
+        if(result.data.message === 'trained'){
+	    console.log('model Trained!!')
+	}
+    }).catch(err => {
+        console.log(err)
+    })
+})
---
+++ server.js
@@ -0,0 +1,117 @@
+require('dotenv').config();
+
+const express  = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const User = require('./models/User.js');
+const path = require('path');
+const cp  = require('child_process');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@face-identification-app-7h6js.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true}, (err, db) =>{
+    mongoose.connection.readyState == 1 ? console.log('CONNECTED TO DB') : console.log('UNABLE TO CONNECT TO DB');
+    if(err) {
+        console.log(err)
+    }
+});
+
+app.post('/api/check_auth', (req, res) => {
+    const secret_key = req.body.secret_key;
+    if(secret_key === process.env.SECRET_KEY){
+        return res.status(200).json({
+            code : true
+        })
+    }
+    return res.status(401).json({
+        code : false
+    })
+})
+app.get('/api/users', (req, res) => {
+    console.log('getting users');
+    User.find()
+    .then(result => {
+        return res.status(200).json({
+            users: result
+        })
+    })
+})
+app.post('/api/add_user', (req, res) => {
+    console.log('req.body.username');
+    const username = req.body.username;
+    if(req.body.secret_key === process.env.SECRET_KEY){
+        User.find()
+        .then(result => {
+            let user_exists = false
+            result.forEach(user => {
+                if(username === user.username){
+                    user_exists = true
+                    res.status(409).json({
+                        message : 'user already exists',
+                        code : 1
+                    })
+                }
+            })
+            if(!user_exists){
+                console.log('creating user')
+                const user = new User({
+                    username : username
+                })
+                user.save()
+                .then((result) => {
+                    console.log('user created!')
+                    res.status(201).json({
+                        response : result
+                    })
+                })
+            }
+        })
+    }else{
+        console.log('wrong secret key')
+        res.status(401).json({
+            message : 'wrong secret key',
+            code : 0
+        })
+    }
+})
+app.post('/api/upload_picture', (req, res) => {
+    const username = req.body.username;
+    const image_path = req.body.image_path;
+    User.findOneAndUpdate({username : username}, {$push : {images_paths : image_path}})
+    .then(() => {
+        res.status(201).json({
+            message : 'added image entry successfully'
+        })
+    }).catch(() => {
+        res.status(500).json({
+            message : 'error'
+        })
+    })
+})
+
+app.get('/api/train', (req, res) =>{
+    console.log('reached api train route');
+    let n = cp.fork('/home/pi/Desktop/face_ID/images/downloader/downloader.js')
+    n.on('message', (m) => {
+	if(m.message === 'downloaded'){
+	    console.log('downloaded')
+	}else if(m.message === 'trained'){
+	    console.log('trained')
+	    res.status(200).json({
+		message : 'trained'
+	    })
+	}
+    })
+})
+
+app.use(express.static('./client'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+});
+
+app.listen(PORT, () => console.log('listening on port ' + PORT))
